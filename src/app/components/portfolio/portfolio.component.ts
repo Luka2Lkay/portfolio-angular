765d4392a004745a21681db6ac6d266716465af8
Luka <lukhanyo95@gmail.com>
2024-07-20 14:56:02 +0200

    add z-index in skills

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { SkillsComponent } from '../skills/skills.component';
-
+import { ProjectsComponent } from '../projects/projects.component';
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [NavbarComponent, HeroSectionComponent, SkillsComponent],
+  imports: [NavbarComponent, ProjectsComponent, HeroSectionComponent, SkillsComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
+
 export class PortfolioComponent {
 
 }
