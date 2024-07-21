001b9016edede758cee6b2f1eee92a76fab12061
Luka <lukhanyo95@gmail.com>
2024-07-21 11:44:00 +0200

    make the project card responsive

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -12,10 +12,15 @@ export class HeroSectionComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     const typed = new Typed('.typing', {
-      strings: ['Hi, there!', "I'm,", 'Lukhanyo Matshebelele'],
+      strings: [`Hi there! ${this.hand},`, "I'm,", `Lukhanyo Matshebelele ${this.smile}`],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
     });
   }
+
+  public laptop: string = '&#128187;'
+  private hand: string = '&#128075;'
+  private smile:string = '&#128526;'
+
 }
