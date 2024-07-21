43cfcb75f0c9608cb84f2d888a7914e070f4eacb
Luka <lukhanyo95@gmail.com>
2024-07-21 15:36:29 +0200

    add the name field

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,30 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent implements OnInit{
-
-
+export class ContactComponent implements OnInit {
   contactForm: FormGroup = new FormGroup({
-    email : new FormControl(''),
+    name: new FormControl(''),
+    email: new FormControl(''),
     subject: new FormControl(''),
-    message: new FormControl('')
-  })
+    message: new FormControl(''),
+  });
 
-  ngOnInit(): void {
-    console.log(environment.SMTP_SERVER)
-  }
+  ngOnInit(): void {}
 
-  submit(): void {
-    console.log(this.contactForm.value)
-  }
-
+  submit(): void {}
 }
