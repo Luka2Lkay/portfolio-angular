fa5d538682fdf835bf43fe7f81776bbc3163cde4
Luka <lukhanyo95@gmail.com>
2024-07-25 15:30:59 +0200

    toggle menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,27 +1,53 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  constructor(private _router: Router) {}
-
   activeSkills: boolean = false;
+  activeProjects: boolean = false;
   activeHome: boolean = true;
+
+  isSvg = true;
+
+  toggleE = false;
+
+  toggle() {
+    if (!this.toggleE) {
+      this.toggleE = true;
+    } else {
+      this.toggleE = false;
+    }
+  }
 
   home(): void {
     window.scrollTo(0, 0);
     this.activeSkills = false;
+    this.activeProjects = false;
     this.activeHome = true;
   }
 
   skills(): void {
     window.scrollTo(0, 600);
     this.activeSkills = true;
+    this.activeHome = false;
+    this.activeProjects = false;
+  }
+
+  projects(): void {
+    window.scrollTo(0, 850);
+    this.activeProjects = true;
+    this.activeSkills = false;
+    this.activeHome = false;
+  }
+
+  contactMe(): void {
+    window.scrollTo(0, 1700);
+    this.activeProjects = false;
+    this.activeSkills = false;
     this.activeHome = false;
   }
 }
