f21db4feccb75b129fa2e17912b04cc49bcccd49
Luka <lukhanyo95@gmail.com>
2024-07-31 16:11:00 +0200

    add a footer

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -4,10 +4,11 @@ import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from "../footer/footer.component";
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [NavbarComponent, ContactComponent, ProjectsComponent, HeroSectionComponent, SkillsComponent],
+  imports: [NavbarComponent, ContactComponent, ProjectsComponent, HeroSectionComponent, SkillsComponent, FooterComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
